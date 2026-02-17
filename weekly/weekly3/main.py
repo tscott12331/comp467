a7734a87cebe890daa77bc90a216e5fb1acaf571
Thomas Scott <t.scott12331@gmail.com>
2026-02-17 11:37:20 -0800

    update weekly3 with comments for submission

--- weekly/weekly3/main.py
+++ weekly/weekly3/main.py
@@ -1,11 +1,13 @@
 import re
 
 with open("output.csv", "w", encoding="utf-8") as output_file:
+    # column name
     output_file.write('Sums')
     with open("lines.txt") as file:
         for i, line in enumerate(file.readlines()):
             matches = re.finditer(r'\d+', line)
 
+            # add numbers on line together
             line_sum = sum([int(num_match.group()) for num_match in matches])
 
             output_file.write(f'\n{line_sum}')
